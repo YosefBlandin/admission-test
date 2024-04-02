99b7a05c17033c1aa0c1cfa3f90c61574468af9c
Yosef Blandin <yosefleanb@gmail.com>
2024-04-01 22:58:46 -0400

    Enabled form view

--- src/Routes.js
+++ src/Routes.js
@@ -5,30 +5,30 @@ import Form from "./containers/Form";
 import { Route, Routes, Outlet } from "react-router-dom";
 
 export default function MyRoutes(props) {
-  const { tableRows, pokemonTypesOptions, handleUpdatePokemonRow } = props;
+	const { tableRows, pokemonTypesOptions, handleUpdatePokemonRow } = props;
 
-  return (
-    <div>
-      <Routes>
-        <Route path="/">
-          <Route index path="/" element={<Home tableRows={tableRows} />} />
-          <Route
-            path="home"
-            element={<Form pokemonTypesOptions={pokemonTypesOptions} />}
-          />
-          <Route
-            path="" // ? wich path?
-            element={
-              <Form
-                pokemonTypesOptions={pokemonTypesOptions}
-                tableRows={tableRows}
-                handleUpdatePokemonRow={handleUpdatePokemonRow}
-              />
-            }
-          />
-          <Route index path="*" element={<Home tableRows={tableRows} />} />
-        </Route>
-      </Routes>
-    </div>
-  );
+	return (
+		<div>
+			<Routes>
+				<Route path="/">
+					<Route index path="/" element={<Home tableRows={tableRows} />} />
+					<Route
+						path="home"
+						element={<Form pokemonTypesOptions={pokemonTypesOptions} />}
+					/>
+					<Route
+						path="form/:name" // ? wich path?
+						element={
+							<Form
+								pokemonTypesOptions={pokemonTypesOptions}
+								tableRows={tableRows}
+								handleUpdatePokemonRow={handleUpdatePokemonRow}
+							/>
+						}
+					/>
+					<Route index path="*" element={<Home tableRows={tableRows} />} />
+				</Route>
+			</Routes>
+		</div>
+	);
 }
